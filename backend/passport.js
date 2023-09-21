c59f19fc98beab620cf7e81c7a0612c427b46bf7
destroyer19-ops <agamahalvin@gmail.com>
2023-09-21 01:36:46 +0100

    bak

--- backend/passport.js
+++ backend/passport.js
@@ -7,7 +7,7 @@ const FACEBOOK_APP_SECRET='5c7dfe0a15f002661cf4af476a7d2a4a'
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:5000/auth/facebook/callback"
+    callbackURL: "https://hs-aexb.onrender.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
